70f144ecc1babf7baea8c81ce0ebb202c68b1f86
vmpardo93 <vicmacondo@hotmail.com>
2025-05-26 14:44:22 -0500

    correction logout

--- ProjectOpp/urls.py
+++ ProjectOpp/urls.py
@@ -37,3 +37,17 @@ urlpatterns = [
     path('organization/<int:pk>/delete/', delete_organization, name='delete_organization'),
     path('profile/edit/', edit_profile, name='edit_profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Configuración adicional para el logout en producción
+if not settings.DEBUG:
+    urlpatterns = [path('', about_us, name='home'),
+                  path('login/', login_view, name='login'),
+                  path('dashboard/', dashboard, name='dashboard'),
+                  path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),  # URL absoluta
+                  path('organizations/', organization_list, name='organization_list'),
+                  path('organization/create/', create_organization, name='create_organization'),
+                  path('organization/<int:pk>/edit/', edit_organization, name='edit_organization'),
+                  path('organization/<int:pk>/delete/', delete_organization, name='delete_organization'),
+                  path('profile/edit/', edit_profile, name='edit_profile'),
+                  path('admin/', admin.site.urls),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
